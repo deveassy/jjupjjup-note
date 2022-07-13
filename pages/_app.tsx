7a0125d339f84ded6bc9d0388e4d006fec16473a
deveassy <xjfmwld@gmail.com>
2022-07-13 23:19:59 +0900

    flag에 따른 header 랜더링

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,23 @@
 import "../styles/globals.css";
 import { AppProps } from "next/app";
 import Header from "../components/header";
+import { useEffect, useState } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [name, setName] = useState<string | null>("");
+  const [flag, setFlag] = useState<string | null>("");
+
+  useEffect(() => {
+    const userName = localStorage.getItem("jjupjjup-user-name");
+    const flag = localStorage.getItem("config-flag");
+
+    setName(userName);
+    setFlag(flag);
+  }, []);
+
   return (
     <>
-      <Header />
+      {!!name && !!flag && <Header />}
       <Component {...pageProps} />
     </>
   );
