0167050deab21b537c46e83c5a43ff6ba1c2b3ad
deveassy <xjfmwld@gmail.com>
2022-07-13 23:56:33 +0900

    header 배치 수정

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,26 +1,36 @@
 import Link from "next/link";
 import styles from "../../styles/Home.module.css";
+import styled from "styled-components";
+import { Container } from "../../styles/styles";
 
 function Header() {
   return (
-    <>
-      <h1 className={styles.title}>
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
-      <Link href="/member">
-        <a>Member</a>
-      </Link>
-      <Link href="/calendar">
-        <a>Calendar</a>
-      </Link>
-      <Link href="/memory">
-        <a>Memory</a>
-      </Link>
-      <Link href="/mukPlace">
-        <a>Muk-place</a>
-      </Link>
-    </>
+    <Container>
+      <h1 className={styles.title}>JJUPJJUP-NOTE</h1>
+      <NavWrapper>
+        <Link href="/member">
+          <a>Member</a>
+        </Link>
+        <Link href="/calendar">
+          <a>Calendar</a>
+        </Link>
+        <Link href="/memory">
+          <a>Memory</a>
+        </Link>
+        <Link href="/mukPlace">
+          <a>Muk-place</a>
+        </Link>
+      </NavWrapper>
+    </Container>
   );
 }
 
 export default Header;
+
+const NavWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  padding: 20px 70px;
+`;
