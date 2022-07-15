acdb853c3f976d50436989f44effa0269ff9f95b
deveassy <xjfmwld@gmail.com>
2022-07-16 00:05:59 +0900

    kakao map 배치 시도

--- pages/mukPlace.tsx
+++ pages/mukPlace.tsx
@@ -1,14 +1,24 @@
 import Link from "next/link";
+import { Container } from "../styles/styles";
+import styled from "styled-components";
+import { useEffect, useState } from "react";
+import MapComponent from "../components/map";
 
 function MukPlace() {
   return (
-    <div>
+    <Container>
       <h1>Muk-Place page</h1>
+      <MapComponent latitude={33.450701} longitude={126.570667} />
       <Link href="/">
         <a>back</a>
       </Link>
-    </div>
+    </Container>
   );
 }
 
 export default MukPlace;
+
+const MapDiv = styled.div`
+  width: 500px;
+  height: 400px;
+`;
