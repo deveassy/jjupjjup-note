26aaf39578c2ac22cb96f59ffa2eacb668fa07d4
deveassy <xjfmwld@gmail.com>
2022-07-16 00:04:54 +0900

    kakao map component 생성

---
+++ components/map/index.tsx
@@ -0,0 +1,48 @@
+import React, { useEffect } from "react";
+import styled from "styled-components";
+
+interface MapProps {
+  latitude: number;
+  longitude: number;
+}
+
+function MapComponent({ latitude, longitude }: MapProps) {
+  useEffect(() => {
+    const mapScript = document.createElement("script");
+
+    mapScript.async = true;
+    mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAOMAP_KEY}&autoload=false`;
+
+    document.head.appendChild(mapScript);
+
+    const onLoadKakaoMap = () => {
+      window.kakao.maps.load(() => {
+        const container = document.getElementById("map");
+        const options = {
+          center: new window.kakao.maps.LatLng(latitude, longitude),
+        };
+        const map = new window.kakao.maps.Map(container, options);
+        const markerPosition = new window.kakao.maps.LatLng(
+          latitude,
+          longitude
+        );
+        const marker = new window.kakao.maps.Marker({
+          position: markerPosition,
+        });
+        marker.setMap(map);
+      });
+    };
+    mapScript.addEventListener("load", onLoadKakaoMap);
+
+    return () => mapScript.removeEventListener("load", onLoadKakaoMap);
+  }, [latitude, longitude]);
+
+  return <MapContainer id="map" />;
+}
+
+const MapContainer = styled.div`
+  width: 300px;
+  height: 400px;
+`;
+
+export default MapComponent;
